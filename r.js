3395ef52075388d399597ed14c2453e789c31535
Eric Hansen <echansen@nabancard.com>
2016-09-20 21:52:44 -0400

    Basic RethinkDB config instantiation!!!!

--- r.js
+++ r.js
@@ -1,3 +1,15 @@
-/**
- * Created by echansen on 16-09-20.
- */
+var config = require(__dirname + '/config.js');
+var r = require('rethinkdb');
+
+var conn = null;
+
+r.connect(config['rethinkdb'], function(err, connection){
+    if (err) { throw err; }
+
+    conn = connection;
+});
+
+module.exports = {
+    'r': r,
+    'conn': conn
+};
